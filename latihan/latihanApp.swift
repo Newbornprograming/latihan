702995aad810ec28cf0244aba4b4f02f4236ef12
Newbornprograming <yohandeny.10@gmail.com>
2024-04-26 14:51:05 +0700

    Initial Commit

---
+++ latihan/latihanApp.swift
@@ -0,0 +1,17 @@
+//
+//  latihanApp.swift
+//  latihan
+//
+//  Created by MacBook Air on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct latihanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
